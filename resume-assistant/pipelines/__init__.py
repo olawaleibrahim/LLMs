0c252cae5c26d19c09f14476030bbba7f9451372
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2025-02-21 16:50:42 +0100

    extract resume content pipeline

--- resume-assistant/pipelines/__init__.py
+++ resume-assistant/pipelines/__init__.py
@@ -1,3 +1,7 @@
 from .scrape_job_description import scrape_webpage_pipeline
+from .extract_resume_content import extract_resume_pipeline
 
-__all__ = ["scrape_webpage_pipeline"]
+__all__ = [
+    "scrape_webpage_pipeline",
+    "extract_resume_pipeline"
+]
---
+++ resume-assistant/pipelines/extract_resume_content.py
@@ -0,0 +1,8 @@
+from resume_assistant.application.rag.retriever import get_langchain_docs
+from resume_assistant.application.rag.utils import extract_content_from_documents
+
+
+def extract_resume_pipeline(user_resume_path):
+    documents = get_langchain_docs(user_resume_path)
+    content = extract_content_from_documents(documents)
+    return content
