a16fc4ea7fdde659ee4a27ee5a3d08be78b0e4b8
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2025-02-21 17:36:18 +0100

    get job description pipeline

--- resume-assistant/tools/run.py
+++ resume-assistant/tools/run.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import click
 from loguru import logger
 
-from pipelines import (scrape_webpage_pipeline, 
+from pipelines import (get_job_description_pipeline, 
                        extract_resume_pipeline)
 
 
@@ -38,7 +38,7 @@ Examples:
     help="Disable caching for the pipeline run.",
 )
 @click.option(
-    "--run-scrape-job-description",
+    "--run-get-job-description",
     is_flag=True,
     default=False,
     help="Whether to run the scrape job description pipeline.",
@@ -61,21 +61,21 @@ Examples:
 )
 def main(
     no_cache: bool = False,
-    run_scrape_job_description: bool = False,
+    run_get_job_description: bool = False,
     run_extract_resume_content: bool = False,
     url: str = "https://job-boards.greenhouse.io/scaleai/jobs/4413274005",
     user_resume_path: str = "Olawale_Machine_Learning_Engineer_Template.pdf",
 ) -> None:
     assert (
-        run_scrape_job_description
+        run_get_job_description
         or run_extract_resume_content
     ), "Please specify an action to run."
 
 
     root_dir = Path(__file__).resolve().parent.parent
 
-    if run_scrape_job_description:
-        job_description = scrape_webpage_pipeline(url)
+    if run_get_job_description:
+        job_description = get_job_description_pipeline(url)
         print(job_description)
 
     if run_extract_resume_content:
