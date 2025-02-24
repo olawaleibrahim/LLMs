aa5951d9980b62b6bf8f1f7b1e0dba0d330f02db
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2025-02-24 21:50:32 +0100

    update to use gemini flash

--- resume-assistant/resume_assistant/application/rag/retriever.py
+++ resume-assistant/resume_assistant/application/rag/retriever.py
@@ -1,9 +1,10 @@
+from langchain_core.document_loaders.blob_loaders import Blob
+from langchain_google_community import DocAIParser
 import os
+import time
+
 from dotenv import load_dotenv
 load_dotenv()
-
-from langchain_google_community import DocAIParser
-from langchain_core.document_loaders.blob_loaders import Blob
 
 
 def get_langchain_docs(user_resume_path):
@@ -12,11 +13,14 @@ def get_langchain_docs(user_resume_path):
     DOC_PROCESSOR_NAME = os.getenv("DOC_PROCESSOR_NAME")
 
     parser = DocAIParser(location="us",
-                     processor_name=DOC_PROCESSOR_NAME,
-                     gcs_output_path="gs://{}/resume-assistant/data/output/dev/pdfs".format(GCS_BUCKET_NAME))
+                         processor_name=DOC_PROCESSOR_NAME,
+                         gcs_output_path="gs://{}/resume-assistant/data/output/dev/pdfs".format(GCS_BUCKET_NAME))
     inp_path = f"gs://{GCS_BUCKET_NAME}/resume-assistant/data/input/dev/pdfs/{user_resume_path}"
     blob = Blob(path=inp_path)
-    docs = list(parser.lazy_parse(blob))
+    operations = parser.docai_parse([blob])
+    while parser.is_running(operations):
+        time.sleep(0.5)
+    results = parser.get_results(operations)
+    docs = list(parser.parse_from_results(results))
 
     return docs
-
